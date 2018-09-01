bc32321df9f5782efc46593a017e37e87b44acfb
Davia <davia@DeNisco.local>
2018-08-31 21:30:25 -0400

    updates throughout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
+import Rectangle from 'react-rectangle';
 import './App.css';
 import 'react-dropdown/style.css';
 const options = ['small', 'medium', 'large', 'extra large'];
@@ -17,66 +18,61 @@ class App extends Component {
       id: 1,
       image: './images/red-logo-shirt.jpg',
       itemName: "COTTON TSHIRT",
-      style: "Style #: MS13KT1906",
-      color: "Color: " + colors[2],
+      style: "MS13KT1906",
+      color: colors[2],
       size: "S",
       quantity: 1,
       price: "$11.00",
+      salePrice: ""
     },
     {
       id: 2,
       image: './images/pink-shirt.jpg',
       itemName: "PRINT GIRL'S TEE",
       style: "Style #: MS13KT1907",
-      color: "Color: " + colors[1],
+      color: colors[1],
       size: "S",
       quantity: 1,
       price: "$17.00",
+      salePrice: ""
     },
     {
       id: 3,
       image: './images/blue-palm-shirt.jpg',
       itemName: "FLOWER PATTERN SHIRT",
       style: "Style #: MS13KT1908",
-      color: "Color: " + colors[2],
+      color: colors[2],
       size: "S",
       quantity: 1,
-      oldPrice: "$21.00",
-      price: "$9.00"
+      price: "$21.00",
+      salePrice: "$9.00",
     },
     {
       id: 4,
       image: './images/plaid-button-shirt.jpg',
       itemName: "CHECK PATTERN TSHIRT",
       style: "Style #: MS13KT1909",
-      color: "Color: " + colors[0],
+      color: colors[0],
       size: "M",
       quantity: 1,
       price: "$22.00",
-    }],
-    options: [{
-      colors: {
-        color1: "red",
-        color2: "blue",
-        color3: "green",
-        color4: "yellow",
-        color5: "pink"
-      },
-      sizes: {
-        small: "small",
-        medium: "medium",
-        large: "large",
-        extraLarge: "extra large"
-      }
+      salePrice: ""
     }]
   };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="app">
+        <div id="allInfo">
+        <header id="header">
           <h1 className="header">{this.state.header}</h1>
           <h5 className="subHeader">{this.state.subHeader}</h5>
+            <ul id="itemsTableHeader">
+              <li>{this.state.items.length} ITEMS</li>
+              <li id="itemsTableHeaderSize">SIZE</li>
+              <li id="itemsTableHeaderQty">QTY</li>
+              <li id="itemsTableHeaderPrice">PRICE</li>
+            </ul>
         </header>
         <div className="container">
           {
@@ -89,32 +85,111 @@ class App extends Component {
                       <img src={item.image} alt="product"/>
                     </div>
 
-                    <div className="col-sm">
+                    <div className="col-sm" id="item-info-main">
                       <li id="itemName">{item.itemName}</li>
+                      <br className="break"/>
+
+                      <div id="itemStyle">
+                        <li id="itemStyle">Style #: {item.style}</li>
+                        <br/>
+                        <li id="color">Color: </li> <li id="itemColor">{item.color}</li>
+                      </div>
+
                       <br/>
-                      <li id="itemStyle">{item.style}</li>
-                      <br/>
-                      <li id="itemColor">{item.color}</li>
-                      <br/>
-                      <a>EDIT</a>
-                      <a>X REMOVE</a>
-                      <a>SAVE FOR LATER</a>
+                      <div id="edit-x-save">
+                        <button type="button" className="btn btn-primary edit" data-toggle="modal" data-target="#exampleModalCenter">
+                          EDIT
+                        </button>
+                        <li className="itemMods">X REMOVE</li>
+                        <li className="itemMods">SAVE FOR LATER</li>
+                      </div>
+
+                      <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div className="modal-dialog modal-dialog-centered" role="document">
+                          <div className="modal-content">
+                            <div className="modal-body">
+
+                              <div className="modal-left col-md">
+                                <div id="modal-div">
+                                  <li className="modal-left" id="modal-itemName">{item.itemName}</li>
+                                    <div id="modal-price-div">
+                                      <li className="modal-left" id="modal-item-price">{item.price || item.salePrice}</li>
+                                    </div>
+                                    <div id="modal-style-div">
+                                      <li className="modal-left" id="modal-item-style">{item.style}</li>
+                                    </div>
+
+                                  <div className="modal-left" id="modal-color-options">
+                                    <div id="modal-color-green" className="modal-color" style={{display: 'inline-block', background: '#add0a5', width: '1.8vw', height: '1vh' }}>
+                                      <Rectangle aspectRatio={[5, 3]}>
+                                        <div style={{ background: '#add0a5', width: '100%', height: '100%' }} />
+                                      </Rectangle>
+                                    </div>
+                                    <div id="modal-color-yellow" className="modal-color" style={{ display: 'inline-block', background: '#f9f8e7', width: '1.8vw', height: '1vh' }}>
+                                      <Rectangle aspectRatio={[5, 3]}>
+                                        <div style={{ background: '#f9f8e7', width: '100%', height: '100%' }} />
+                                      </Rectangle>
+                                    </div>
+                                    <div id="modal-color-blue" className="modal-color" style={{ display: 'inline-block', background: '#2f69b7', width: '1.8vw', height: '1vh' }}>
+                                      <Rectangle aspectRatio={[5, 3]}>
+                                        <div style={{ background: '#2f69b7', width: '100%', height: '100%' }} />
+                                      </Rectangle>
+                                    </div>
+                                  </div>
+                                  <div id="modal-color-choice">
+                                    <li className="modal-left" id="modal-item-selected-color">Color: {item.color}</li>
+                                  </div>
+                                  <div id="modal-size-selection">
+                                    <li className="modal-left" id="modal-item-size">
+                                      <select>
+                                        <option selected value="small">small</option>
+                                        <option value="medium">medium</option>
+                                        <option value="large">large</option>
+                                        <option value="extra large">extra large</option>
+                                      </select>
+                                    </li>
+                                    <li className="modal-left" id="modal-item-qty">
+                                      <form id="modal-form">
+                                        <label htmlFor="quantity"></label>
+                                        <input type="number" id="modal-quantity" name="quantity" min="1" max="99" step="1" value="1"/>
+                                      </form>
+                                    </li>
+                                  </div>
+                                  <div id="modal-edit-button">
+                                    <button type="button" className="btn btn-primary modal-left" id="modal-edit">EDIT</button>
+                                  </div>
+                                  <div id="modal-item-details-link">
+                                    <li className="modal-left" id="modal-item-product-details">Check product details</li>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div className="modal-right col-md">
+                                <button type="button" className="btn btn-secondary" id="modal-close" data-dismiss="modal">ｘ</button>
+                                <img className="modal-right" id="modal-item-image" src={item.image} alt="product"/>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
 
-                    <div className="col-">
-                      <li id="itemSize">{item.size}</li>
-                    </div>
-                    <div className="col-">
-                      <form>
-                        <label for="quantity"></label>
-                        <input type="number" id="quantity" name="quantity" min="1" max="99" step="1" value="1"/>
-                      </form>
-                    </div>
-                    <div className="col-" id="itemPrice">
-                      <li id="itemPrice">{item.price}</li>
+                    <div id="size-qty-price">
+                      <div className="col-">
+                        <li id="itemSize">{item.size}</li>
+                      </div>
+                      <div className="col-">
+                        <form id="form">
+                          <label htmlFor="quantity"></label>
+                          <input type="number" id="quantity" name="quantity" min="1" max="99" step="1" value="1"/>
+                        </form>
+                      </div>
+                      <div className="col-" id="itemPrice">
+                        <li id="itemPrice">{item.price}</li>
+                      </div>
+                      </div>
                     </div>
 
-                  </div>
 
               )
             })
@@ -140,6 +215,7 @@ class App extends Component {
           </div>
         </div>
       </div>
+    </div>
     );
   }
 }
